9ca30943b1329f733fe7fe6fafa5d2a869bbe766
Parker Catalano <catalano.work@gmail.com>
2017-12-05 15:58:22 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
-def create_project_hash
-  # write your code here
+class KickstarterScraper
+
+  def create_project_hash
+    html = File.read('./fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
+    binding.pry
+
+    # projects: kickstarter.css("li.project.grid_4")
+    # title: project.css("h2.bbcard_name strong a").text
+  end
 end
+
+KickstarterScraper.new.create_project_hash
